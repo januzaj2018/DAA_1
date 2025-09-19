77382a7c12598d1479e464800bea433708dd62a4
januzaj2018 <aibekcsgo@gmail.com>
2025-09-19 15:19:31 +0500

    feat(metrics): counters, depth tracker

---
+++ src/main/java/org/example/algorithms/util/Metrics.java
@@ -0,0 +1,51 @@
+package org.example.algorithms.util;
+
+public class Metrics {
+    private long comparisons;
+    private long allocations;
+    private int MaxRecursionDepth;
+    private int currentRecursionDepth;
+    private long startTime;
+    private long endTime;
+
+    public void start(){
+        this.startTime = System.nanoTime();
+        this.comparisons = 0;
+        this.allocations = 0;
+        this.MaxRecursionDepth = 0;
+        this.currentRecursionDepth = 0;
+    }
+    public void stop(){
+        this.endTime = System.nanoTime();
+    }
+    public long getElapsedTime(){
+        return this.endTime - this.startTime;
+    }
+    public void trackRecursionStart(){
+        this.currentRecursionDepth++;
+        if(this.currentRecursionDepth > this.MaxRecursionDepth){
+            this.MaxRecursionDepth = this.currentRecursionDepth;
+        }
+    }
+    public void trackRecursionEnd(){
+        this.currentRecursionDepth--;
+    }
+    public void comparisonIncrement(){
+        this.comparisons++;
+    }
+    public void allocationIncrement(){
+        this.allocations++;
+    }
+
+    public long getComparisons() {
+        return comparisons;
+    }
+
+    public long getAllocations() {
+        return allocations;
+    }
+
+    public int getMaxRecursionDepth() {
+        return MaxRecursionDepth;
+    }
+}
